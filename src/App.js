0aeb615cd1dca6ab042caf2a82facdcc51405064
Yudai <things.without.color@gmail.com>
2020-07-17 08:22:38 +0900

    useMemo実装

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import AppContext from './contexts/AppContext';
 // import BasicReducer from './components/BasicReducer';
 import CompB from './components/CompB';
 import {useReducer} from 'react'
+import Memo from './components/Memo';
 
 const initialState = 0
 const reducer = (currentState, action) => {
@@ -40,8 +41,9 @@ function App() {
           {/* <ApiFetch />
           <B /> */}
           {/* <BasicReducer /> */}
-          Count {count}
-          <CompB />
+          {/* Count {count}
+          <CompB /> */}
+          <Memo />
         </header>
       </div>
     </AppContext.Provider>
